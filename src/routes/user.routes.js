33797c558477ec8c10a013a010fc45fbb61976de
nisarg-frontendarmy <nisarg.frontendarmy@gmail.com>
2024-01-17 13:08:38 +0530

    Add new routes user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistroy,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updatedAccountDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -26,11 +33,19 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
-
 // secured routes
-
-
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updatedAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistroy);
 
 export default Router;
